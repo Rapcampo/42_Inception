93a4968627b0c13d22d485c7180eba2bebd55fda
rapcampo <rapcampo@student.42porto.com>
2026-01-25 21:46:10 +0000

    finally made mariadb work

--- srcs/requirements/mariadb/dependancies/script.sh
+++ srcs/requirements/mariadb/dependancies/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DATADIR=/var/lib/mysql
+DATADIR=/run/mysql
 SOCKET=/run/mysql/mysqld.sock
 DB_ROOT_PASSWORD="$(cat /run/secrets/db_root_pass)"
 WP_DB_PASSWORD="$(cat /run/secrets/db_pass)"
@@ -21,17 +21,17 @@ if [ ! -d "$DATADIR" ]; then
 		sleep 1
 	done
 
-	mariadb -uroot -p"$DB_ROOT_PASSWORD" <<EOSQL
+	mariadb --protocol=socket --socket="$SOCKET" -uroot -p"$DB_ROOT_PASSWORD" <<EOSQL
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-	CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;
+	CREATE DATABASE IF NOT EXISTS \`$WP_DB_NAME\`;
 	CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';
-	GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO '${WP_DB_USER}'@'%';
+	GRANT ALL PRIVILEGES ON \`$WP_DB_NAME\`.* TO '${WP_DB_USER}'@'%';
 	FLUSH PRIVILEGES;
 EOSQL
 
-	mysqladmin --protocol=socket --socket="$SOCKET" -u root -p"${DB_ROOT_PASSWORD}" shutdown
+	mysqladmin --protocol=socket --socket="$SOCKET" -u root -p"$DB_ROOT_PASSWORD" shutdown
 	wait "$pid"
 fi
 
 exec gosu mysql mariadbd \
-	--datadir=/var/lib/mysql --bind-address=0.0.0.0
+	--datadir="$DATADIR" --bind-address=0.0.0.0
