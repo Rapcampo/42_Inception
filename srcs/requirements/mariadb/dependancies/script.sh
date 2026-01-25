af8da7097bd46210f95c2fcab0165d1a87d0c888
rapcampo <rapcampo@student.42porto.com>
2026-01-25 17:27:08 +0000

    trying to fix user creation

--- srcs/requirements/mariadb/dependancies/script.sh
+++ srcs/requirements/mariadb/dependancies/script.sh
@@ -2,32 +2,41 @@
 
 set -e
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+DATADIR=/var/lib/mysql
+SOCKET=/run/mysql/mysqld.sock
+DB_ROOT_PASSWORD="$(< /run/secrets/db_root_pass)"
+WP_DB_PASSWORD="$(< /run/secrets/db_pass)"
+
+:"${WP_BD_NAME:?WP_DB_NAME not set}"
+:"${WP_BD_PASSWORD:?WP_DB_NAME not set}"
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld "$DATADIR"
+
+if [ ! -d "$DATADIR/mysql" ]; then
 	echo "Starting MariaDB"
 
-	DB_ROOT_PASSWORD="$(< /run/secrets/db_root_pass)" \
-	WP_DB_PASSWORD="$(< /run/secrets/db_pass)"
-
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-	chown -R mysql:mysql /var/lib/mysql
+	mariadb-install-db --user=mysql --datadir="$DATADIR"
+	chown -R mysql:mysql "$DATADIR"
 
 	gosu mysql mariadbd \
-		--datadir=/var/lib/mysql \
-		--skip-networking --socket=/run/mysqld/mysql.sock & pid="$!"
+		--datadir="$DATADIR" \
+		--skip-networking --socket="$SOCKET" & pid="$!"
 
-	until mysqladmin ping --socket=/run/mysqld/mysql.sock --silent; do
+	until mysqladmin --protocol=socket --socket="$SOCKET" ping --silent; do
 		sleep 1
 	done
 
-	mariadb -uroot -p"$DB_ROOT_PASSWORD" -e "
-	CREATE DATABASE IF NOT EXISTS \`$WP_DB_NAME\`;
-	CREATE USER IF NOT EXISTS '\`'$WP_DB_USER'\`'@'\`'%'\`' IDENTIFIED BY '\`'$WP_DB_PASSWORD'\`';
-	GRANT ALL PRIVILEGES ON '\`'$WP_DB_NAME'\`'.* TO '\`'$WP_DB_USER'\`'@'\`'%'\`';
+	mariadb --protocol=socket --socket="$SOCKET" -u root <<EOSQL
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+	CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;
+	CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';
+	GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO '${WP_DB_USER}'@'%';
 	FLUSH PRIVILEGES;
-	"
+EOSQL
 
-	mysqladmin shutdown
-	#kill "$pid"
+	mysqladmin --protocol=socket --socket="$SOCKET" -u root -p"${DB_ROOT_PASSWORD}" shutdown
+	wait "$pid"
 fi
 
 exec gosu mysql mariadbd \
